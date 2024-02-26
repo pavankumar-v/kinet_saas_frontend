ef608ce9e874bf9abada2123d579646f40417007
pavankumar-v <pavankumarv124@gmail.com>
2024-02-26 12:26:19 +0545

    typescript and tailwindcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
