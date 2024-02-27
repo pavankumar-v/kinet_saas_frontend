b6deaf05746debd9800999118ce11a24e3a5b4ae
pavankumar-v <pavankumarv124@gmail.com>
2024-02-28 00:16:51 +0545

    auth0 setup for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 import "./App.css";
-import { Button } from "@/components/ui/button";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import LoginButton from "@components/auth/LoginButton";
+import LogoutButton from "@components/auth/LogoutButton";
+import UserInfo from "@components/userInfo/UserInfo";
 
 function App() {
   return (
     <div className="h-screen">
-      <Button variant="outline">Button</Button>
-
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>This action cannot be undone. This will permanently delete your account and remove your data from our servers.</AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <LoginButton />
+      <LogoutButton />
+      <UserInfo />
     </div>
   );
 }
