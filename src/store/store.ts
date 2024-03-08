57d0d93d345c62cc0c8162409f9545d82de904af
pavankumar-v <pavankumarv124@gmail.com>
2024-03-08 09:05:02 +0530

    Basic Foundation Redux Setup ✨

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import exampleReduce from "@/features/example/exampleSlice";
+
+const middlewareConfiguration = { serializableCheck: false };
+
+export default configureStore({
+  reducer: { exampleReduce },
+  devTools: {
+    name: "kinet saas",
+  },
+});
+
+
+// reference https://dev.to/raaynaldo/redux-toolkit-setup-tutorial-5fjf
