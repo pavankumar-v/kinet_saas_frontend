35831e0787060caa24126f4ee1e67f0ddbfacb39
pavankumar-v <pavankumarv124@gmail.com>
2024-03-21 02:58:58 +0530

    getting access token

--- src/components/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,38 @@
 import LogoutButton from '@/components/auth/LogoutButton'
+import { Button } from '@/components/ui/button'
+import AUTH0_CONFIG from '@/config/auth'
+import { useAuth0 } from '@auth0/auth0-react'
+import { config } from 'localforage'
 import { LogOut } from 'lucide-react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type Props = {}
 
 const Dashboard: React.FC<Props> = (props) => {
+  const { user, isAuthenticated, getAccessTokenSilently, getAccessTokenWithPopup, getIdTokenClaims } = useAuth0();
+  useEffect(() => {
+    (async () => {
+      try {
+        const id_token = await getIdTokenClaims();
+        console.log(id_token)
+        const token = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: "https://kinet-dev1024.us.auth0.com/api/v2/",
+            scope: "read:current_user"
+          },
+        });
+        console.log(token)
+      } catch (e) {
+        // Handle errors such as `login_required` and `consent_required` by re-prompting for a login
+        // getAccessTokenWithPopup()
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
+
   return (
     <div>Dashboard
-      <LogoutButton />
+      <Button onClick={()=>getAccessTokenWithPopup()}> Get Token</Button>
     </div>
   )
 }
