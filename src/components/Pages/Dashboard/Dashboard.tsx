ef209ca2c0d2e6b06890df5035149d0f6959acf7
pavankumar-v <pavankumarv124@gmail.com>
2024-03-23 09:38:39 +0530

    adding logout button on dashboard page

--- src/components/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import LogoutButton from '@/components/auth/LogoutButton'
+import React from 'react'
 
 type Props = {}
 
 const Dashboard: React.FC<Props> = (props) => {
   return (
     <div>Dashboard
+      <LogoutButton />
     </div>
   )
 }
